03137dae08accaa92f1ef9d5fc8118ea9074fecd
Jeffery McLean <jeffery.mclean@gmail.com>
2013-11-06 23:04:37 +0000

    second commit: extract objects from weather site using JSON

--- app.js
+++ app.js
@@ -1,11 +1,37 @@
 $.getJSON(
-		'http://autocomplete.wunderground.com/aq?query=&cb=?',
+		'http://autocomplete.wunderground.com/aq?query=vanc&cb=?',
+
 
 	function(data) {
 		console.log(data);
+
+		var cities = data['RESULTS'][0];//["type"]; 
+			console.log(cities);
+		// var cities = type[1]; 
+		// for(var i=0; i< type.length; i++) {
+		// 	var type = type[i]; 
+		// 	console.log(type); 
+		// 	var li = $('<li></li>'); 
+
+
 
 	}
 
 	
 
-	)
+// var movies = data["movies"]; 
+//             var movie = movies[0];
+//             for(var i=0; i< movies.length; i++) {
+//                 var movie = movies[i]; 
+//                 console.log(movie); 
+//                 var li = $('<li></li>'); 
+
+
+
+
+
+	
+)
+
+	
+
