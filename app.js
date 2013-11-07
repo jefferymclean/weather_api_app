fedce1128761675459e87d293df5b1952089ac84
Jeffery McLean <jeffery.mclean@gmail.com>
2013-11-07 04:32:18 +0000

    fix list! And display results on page to user

--- app.js
+++ app.js
@@ -1,57 +1,82 @@
 $(function() {
 
-	$.getJSON(
-			'http://autocomplete.wunderground.com/aq?query=vanc&cb=?',
+	$('#btn-add-item').click( function() {
+	
+		// we're collecting the input value (.val) from  
+		// the user and putting it in the bla
+		bla = $('#txt_name').val(); 
+
+		$.getJSON(
+			'http://autocomplete.wunderground.com/aq?query=' + bla + '&cb=?',
+			function(data) {
+				console.log(data); 
+
+					// results is an array (a collection of cities)
+				var cities = data['RESULTS']; 
+				console.log(cities);
+				//var cities = cities[]; 
+
+				for (var i=0; i< cities.length; i++) {
+					var city = cities[i]; 
+					console.log(city);
+
+					var cityName =  '<li>' + city['name'] + '</li>'; 
+					console.log(cityName);
+
+					$('#list').append(cityName); 
 
 
-		function(data) {
-			console.log(data);
-
-			var cities = data['RESULTS']; 
-			console.log(cities);
-			//var cities = cities[]; 
-			for (var i=0; i< cities.length; i++); {
-				//	var type = type[i]; 
-				//	console.log(cities); 
-				//	var li = $('<li></li>'); 
 
 
-
-
-				$('#btn-add-item').click( function() {
-		
-					// we're collecting the input value (.val) from  
-					// the user and putting it in the bla
-					bla = $('#txt_name').val();
-					$("#list").append("<li>" + bla + "</li>");
-					//setting the user input (#txt_name) to empty string
-    				$("#txt_name").val("");
-				});
-				
+						
+    			}
 			}
-	 	}			
-	)
+		);
+	});
+}); 
 
-});		
+			// var movies = data["movies"]; 
+	  //           var movie = movies[0];
+	  //           for(var i=0; i< movies.length; i++) {
+	  //               var movie = movies[i]; 
+	  //               console.log(movie); 
+	  //               var li = $('<li></li>'); 
+	  //               // alternative: var imageSrc = movie.posters.profile
+	                
+	  //                   var imageSrc = movie['posters']['profile'];                               
+	  //                   var img= $('<img />').attr(
+	  //                       'src', imageSrc
+	  //                   );
+	  //                   li.append(img); 
+	  //                   li.append('<div>'+movie['title']+'</div>');
+	  //                  $('#movies').append(li); 
+         //   }    
+   //      }
+   
+// )
+
+					//$("#txt_name").val(""); 
+				
+
 
 					// door.open
 	 				//object.action (on the object)
 	 						//action = function or method
 
 
-	// var movies = data["movies"]; 
-	//             var movie = movies[0];
-	//             for(var i=0; i< movies.length; i++) {
-	//                 var movie = movies[i]; 
-	//                 console.log(movie); 
-	//                 var li = $('<li></li>'); 
+					// var movies = data["movies"]; 
+					//             var movie = movies[0];
+					//             for(var i=0; i< movies.length; i++) {
+					//                 var movie = movies[i]; 
+					//                 console.log(movie); 
+					//                 var li = $('<li></li>'); 
 
 
-		// var cities = type[1]; 
-			// for(var i=0; i< type.length; i++) {
-			// 	var type = type[i]; 
-			// 	console.log(type); 
-			// 	var li = $('<li></li>'); 
+					// var cities = type[1]; 
+						// for(var i=0; i< type.length; i++) {
+						// 	var type = type[i]; 
+						// 	console.log(type); 
+						// 	var li = $('<li></li>'); 
 
 
 		
